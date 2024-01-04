b0d643766e345f48ce46a89288018724d526c0b7
Kevs <79332227+kevsmartini@users.noreply.github.com>
2024-01-04 21:59:28 +0000

    code totally changed and refactored, I have modified the code in general, it needs to de fisnised still

--- script.js
+++ script.js
@@ -1,44 +1,149 @@
-const numberOne = 3
-const numberTwo = 5
-const operator = ''
-
 const display = document.querySelector('.display')
-const btnOne = document.querySelector('.one')
-const bntTwo = document.querySelector('.two')
+const acumulator = document.querySelector('.acumulator')
+
+
+const clear = document.querySelector('.clear')
+const remove = document.querySelector('.remove')
+const decimal = document.querySelector('.decimal')
+const equals = document.querySelector('.equals')
+
+
+
+const btnNumbers = document.querySelectorAll('.number')
+const btnOperators = document.querySelectorAll('.operator')
+
+
+let numberOne = parseInt(acumulator.textContent)
+let numberTwo = parseInt(display.value)  
+let operator = ''
+let currentNumber = ''
+let acumulatorNumber = ''
+
+
+
 
 /////////////OPERATORS//////////
 
-const add = (n1, n2) => {
-   return n1+n2
-}
 
-const subtract = (n1, n2) => {
-
-}
-
-const multiply = (n1, n2) => {
-
-}
-
-const divide = (n1, n2) => {
-
-}
+// const add = (n1, n2) => {
+//    return n1+n2
+// }
 
 
+// const subtract = (n1, n2) => {
+//     return n1-n2
 
-const operate= (n1, n2)=>{
-    return add(n1,n2)
-}
 
-console.log(operate(numberOne,numberTwo))
+// }
+
+
+// const multiply = (n1, n2) => {
+//     return n1*n2
+
+// }
+
+
+// const divide = (n1, n2) => {
+//     return n1/n2
+
+
+// }
 
 
 //////BUTTONS///
 
-btnOne.addEventListener('click',(event)=>{
-display.textContent = 1
+btnNumbers.forEach((number) => number.addEventListener('click',(e) => {
+ 
+  numberClicked(e.target.textContent)
+  display.value = currentNumber
+  
+
+}))
+
+btnOperators.forEach((operator) => operator.addEventListener('click',(e)=>{
+  
+  if(currentNumber != '' ){
+    
+    operatorClicked( e.target.textContent)
+
+}
+  
+  
+
+
+
+}))
+
+clear.addEventListener('click',()=>{
+
+  display.value = ''
+  acumulator.textContent = ''
+  currentNumber = ''
+  operator= ''
+  acumulatorNumber = ''
+
+
 })
 
-bntTwo.addEventListener('click',(event)=>{
-    display.textContent = 2
-    })
+equals.addEventListener('click',()=>{
+
+  if(currentNumber != '' && acumulatorNumber != ''){
+      operate()
+
+    display.value = acumulatorNumber
+    acumulator.textContent = ''
+    currentNumber = ''
+
+  }
+
+  
+
+
+})
+
+
+
+const numberClicked = (number) => {
+  currentNumber += number
+
+}
+
+const operatorClicked = (op) => {
+
+  operator = op
+  acumulatorNumber = currentNumber
+  currentNumber = ''
+  acumulator.textContent = acumulatorNumber + operator
+  display.value = currentNumber
+
+}
+
+const operate = () =>{
+
+  currentNumber = Number(currentNumber)
+  acumulatorNumber = Number(acumulatorNumber)
+
+  if(operator === '+') acumulatorNumber += currentNumber;
+  if(operator === '-') acumulatorNumber -= currentNumber;
+  if(operator === 'x') acumulatorNumber *= currentNumber;
+  if(operator === '÷') acumulatorNumber /= currentNumber;
+
+  currentNumber = currentNumber.toString()
+  acumulatorNumber = acumulatorNumber.toString()
+ 
+}
+
+
+
+
+
+
+remove.addEventListener('click',()=>{
+
+  display.value = display.value.slice(0,-1)
+
+  currentNumber = display.value
+
+})
+
+
